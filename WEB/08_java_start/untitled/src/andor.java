f198eb2fed21ae481659ba144c7c9676cdf0de1b
supersfel <jmg081005@naver.com>
2023-01-23 22:17:33 +0900

    논리연산자

---
+++ WEB/08_java_start/untitled/src/andor.java
@@ -0,0 +1,11 @@
+public class andor {
+    public static void main (String[] args) {
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        System.out.println(b % c == 1 );
+        System.out.println(b<c && a>b);
+        System.out.println(b<c || a>b);
+
+    }
+}
