fe5cfcd4da1089ffe5fa9f0004bd9c8a0e4e6c23
supersfel <81850778+supersfel@users.noreply.github.com>
2023-02-26 13:16:21 +0900

    ai 업데이트

--- AI/chromedriver_win32/coffibin_crawling.py
+++ AI/chromedriver_win32/coffibin_crawling.py
@@ -4,7 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-wd = webdriver.Chrome('/chromedriver.exe')
+wd = webdriver.Chrome('chromedriver.exe')
 wd.get("https://www.coffeebeankorea.com/store/store.asp")
 
 wd.execute_script("storePop2('31')")
@@ -13,6 +13,6 @@ time.sleep(10)
 
 text = wd.find_element(By.XPATH, r'//*[@id="matizCoverLayer0Content"]/div/div/div[2]/table/tbody[1]/tr[3]/td').text
 print(text)
-while (True):
-    pass
+# while (True):
+#     pass
 
