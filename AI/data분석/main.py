4ec0a02278b18fbab56f8342e10bd4a7124d31b1
supersfel <81850778+supersfel@users.noreply.github.com>
2023-03-12 12:03:55 +0900

    분석

--- AI/data분석/main.py
+++ AI/data분석/main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('시군구_인구동태건수_및_동태율_출생_사망_혼인_이혼__20230225113457.csv',encoding='cp949')
 
+
+
 df = df.T
 df.columns = df.iloc[0].to_numpy()
 df = df.drop('행정구역별')
--- AI/titanic/main.py
+++ AI/titanic/main.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('train.csv')
 print(df)
-# print(df['Survived'].value_counts())
+print(df['Survived'].value_counts())
 
 df['Survived'] = df['Survived'].replace(0,'Perish')
 df['Survived'] = df['Survived'].replace(1,'Survived')
 
-# print(df)
+print(df)
 
 
 df['Pclass'] = df['Pclass'].replace(1,'1st')
 df['Pclass'] = df['Pclass'].replace(2,'2nd')
 df['Pclass'] = df['Pclass'].replace(3,'3rd')
 
-# print(df)
+print(df)
 
 sns.countplot(data = df,x='Survived',hue = 'Pclass')
 plt.show()
